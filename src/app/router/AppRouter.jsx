4bab2cd5ffd7f35393814a2b1cd56d5981d7f98e
Md Ashaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-07-07 14:31:43 +0600

    Add router

---
+++ src/app/router/AppRouter.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import AdminLayout from "../../layouts/AdminLayout";
+
+import Dashboard from "../../pages/Dashboard";
+import InvoiceCreate from "../../pages/invoices/InvoiceCreate";
+
+import AddProduct from "../../pages/products/AddProduct";
+import ViewAllProducts from "../../pages/products/ViewAllProducts";
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AdminLayout />}>
+          <Route path="/" element={<Dashboard />} />
+
+          {/* Invoice */}
+          <Route path="/invoices/create" element={<InvoiceCreate />} />
+
+          {/* Products */}
+          <Route path="/products/create" element={<AddProduct />} />
+          <Route path="/products" element={<ViewAllProducts />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRouter;

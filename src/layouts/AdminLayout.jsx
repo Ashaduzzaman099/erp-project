65b257f68709f95b6aebef44f0bc2b488b72653b
Md Asaduzzaman Ashik <www.ashaduzzaman25@gmail.com>
2026-08-28 23:32:35 +0600

    Add Search tool

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -7,10 +7,8 @@ const AdminLayout = () => {
   return (
     <div className="flex h-screen bg-gray-100">
       <Sidebar />
-
       <div className="flex flex-1 flex-col">
         <Navbar />
-
         <main className="flex-1 overflow-y-auto p-4">
           <Outlet />
         </main>
